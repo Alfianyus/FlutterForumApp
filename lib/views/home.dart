d19846912281c230440e78327c99d2c8cdce0dd3
Alfianyus <alfian.yuska1@gmail.com>
2023-12-14 16:13:52 +0700

    first commit

---
+++ lib/views/home.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'widgets/post_field.dart';
+import 'widgets/post_data.dart';
+import 'package:forumapp/controllers/post_controller.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final PostController _postController = Get.put(PostController());
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Forum APP'),
+        backgroundColor: Colors.black,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              PostField(
+                hintText: 'what do you want to ask?',
+                controller: _textController,
+              ),
+              // const SizedBox(
+              //   height: 20,
+              // ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  elevation: 0,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 45,
+                    vertical: 10,
+                  ),
+                ),
+                onPressed: () async {
+                  await _postController.createPost(
+                    content: _textController.text.trim(),
+                  );
+                  _textController.clear();
+                  _postController.getAllPosts();
+                },
+                child: Obx(() {
+                  return _postController.isLoading.value
+                      ? const CircularProgressIndicator()
+                      : Text('Post');
+                }),
+              ),
+              const SizedBox(
+                height: 0,
+              ),
+              Text('Posts'),
+              const SizedBox(
+                height: 20,
+              ),
+              Obx(() {
+                return _postController.isLoading.value
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: _postController.posts.value.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: postdata(
+                              post: _postController.posts.value[index],
+                            ),
+                          );
+                        },
+                      );
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/widgets/post_data.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:forumapp/controllers/post_controller.dart';
+import 'package:forumapp/models/post_model.dart';
+import 'package:forumapp/views/post_details.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class postdata extends StatefulWidget {
+  const postdata({
+    super.key,
+    required this.post,
+  });
+
+  final PostModel post;
+
+  @override
+  State<postdata> createState() => _postdataState();
+}
+
+class _postdataState extends State<postdata> {
+  final PostController _postController = Get.put(PostController());
+  Color likedPost = Colors.black;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.blue[200],
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.post.user!.name!,
+            style: GoogleFonts.poppins(),
+          ),
+          Text(
+            widget.post.user!.email!,
+            style: GoogleFonts.poppins(
+              fontSize: 10,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            widget.post.content!,
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () async {
+                  await _postController.likeAndDislike(widget.post.id);
+                  _postController.getAllPosts();
+                },
+                icon: Icon(
+                  Icons.thumb_up,
+                  color: widget.post.liked! ? Colors.blue : Colors.black,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  Get.to(
+                    () => PostDetails(
+                      post: widget.post,
+                    ),
+                  );
+                },
+                icon: Icon(Icons.message),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
